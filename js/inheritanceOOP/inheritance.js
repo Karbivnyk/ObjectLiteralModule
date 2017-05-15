3bc346e48e2ae9a714033b279b3b069b57162997
www <www@www.ru>
2017-05-15 19:30:21 +0300

    Inharitance in JS

---
+++ js/inheritanceOOP/inheritance.js
@@ -0,0 +1,52 @@
+//instance - екземпляр
+//Inheritance - наслідування
+//classical prototypal programing
+//
+
+//	Inheritance
+//	
+function inherits(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+};
+
+
+var Person = function(name) {
+	this.name = name;
+}
+
+Person.prototype.sayName = function() {
+	console.log( "My name is " + this.name);
+};
+
+var john = new Person("John");
+var bobby = new Person("Bobby");
+
+john.sayName();
+
+
+// new Obj
+var Musician = function(name, instument) {
+	//Person.call(this, name)
+	Musician.super_ .call(this, name);
+	this.instrument = instument;
+}
+
+inherits(Musician, Person);
+
+
+//new Method for Musician obj  
+Musician.prototype.getInstrument = function() {
+	console.log(this.instrument);
+};
+
+var julia = new Musician("julia", "trombone");
+julia.sayName();
+julia.getInstrument();
